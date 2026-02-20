bd7db502379776ce06220935ae103cf0657aad51
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 19:25:58 +0000

    9

--- api/server.js
+++ api/server.js
@@ -1,5 +1,28 @@
-import { handler as ssrHandler } from '../dist/ssr/server/server-entry.js';
+import render from '../dist/ssr/server/server-entry.js';
 
 export default async function handler(req, res) {
-  await ssrHandler(req, res);
+  const ssrContext = {
+    req,
+    res,
+    url: req.url
+  };
+
+  try {
+    const app = await render(ssrContext);
+    
+    const { renderToString } = await import('vue/server-renderer');
+    const html = await renderToString(app);
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.end(html);
+  } catch (error) {
+    if (error.url) {
+      res.writeHead(error.code || 302, { Location: error.url });
+      res.end();
+    } else {
+      console.error(error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  }
 }

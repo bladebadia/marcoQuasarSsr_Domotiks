8400eba02311b12776315e3ae89a599d5d17bbaf
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 21:55:41 +0000

    26

--- api/server.js
+++ api/server.js
@@ -4,12 +4,8 @@ import ssrHandler from '../dist/ssr/server/server-entry.js';
 const app = express();
 
 app.all(/.*/, async (req, res) => {
-  // Adaptar req para asegurar que tenga headers (compatibilidad Vercel/Quasar SSR)
-  if (!req.headers && req.getHeaders) {
-    req.headers = req.getHeaders();
-  } else if (!req.headers) {
-    req.headers = {};
-  }
+  // Asegurar que req.headers exista
+  req.headers = req.headers || {};
   await ssrHandler(req, res);
 });
 

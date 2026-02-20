e61b78b3e1014af9aad5e7fc0d9fdb59eb2c8680
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 21:37:46 +0000

    19

--- api/server.js
+++ api/server.js
@@ -8,18 +8,31 @@ function buildHeaders(rawHeaders) {
   return headers;
 }
 
+function deepLog(obj, path = 'req', depth = 2) {
+  if (depth < 0) return;
+  if (obj && typeof obj === 'object') {
+    for (const key of Object.keys(obj)) {
+      if (key === 'headers') {
+        console.log(`${path}.${key}:`, obj[key]);
+      }
+      if (typeof obj[key] === 'object') {
+        deepLog(obj[key], `${path}.${key}`, depth - 1);
+      }
+    }
+  }
+}
+
 export default async function handler(req, res) {
-  // Asegura headers en req
   if (!req.headers && req.rawHeaders) {
     req.headers = buildHeaders(req.rawHeaders);
   }
-  // Asegura headers en req.request
   if (req.request && !req.request.headers && req.rawHeaders) {
     req.request.headers = buildHeaders(req.rawHeaders);
   }
-  // Asegura headers en req.connection
   if (req.connection && !req.connection.headers && req.rawHeaders) {
     req.connection.headers = buildHeaders(req.rawHeaders);
   }
+  // Loguear en profundidad
+  deepLog(req);
   await ssrHandler(req, res);
 }

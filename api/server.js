fe6ddb6f2faae368897da1a6a228919f232b24a7
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 18:40:54 +0000

    4

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
-import { ssrProductionExport } from '../dist/ssr/server/server-entry.js';
+import ssrHandler from '../dist/ssr/server/server-entry.js';
 
 export default async function handler(req, res) {
-  const { handler: ssrHandler } = ssrProductionExport;
-  await ssrHandler(req, res);
+  console.log('Tipo:', typeof ssrHandler);
+  console.log('Es función:', typeof ssrHandler === 'function');
+  
+  if (typeof ssrHandler === 'object') {
+    console.log('Keys:', Object.keys(ssrHandler));
+  }
+  
+  res.status(200).json({ 
+    tipo: typeof ssrHandler,
+    keys: typeof ssrHandler === 'object' ? Object.keys(ssrHandler) : null
+  });
 }

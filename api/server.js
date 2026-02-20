d1301ff09fc364a3189c6339cbaab6d142fe1bbe
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 21:35:13 +0000

    18

--- api/server.js
+++ api/server.js
@@ -9,8 +9,17 @@ function buildHeaders(rawHeaders) {
 }
 
 export default async function handler(req, res) {
+  // Asegura headers en req
   if (!req.headers && req.rawHeaders) {
     req.headers = buildHeaders(req.rawHeaders);
+  }
+  // Asegura headers en req.request
+  if (req.request && !req.request.headers && req.rawHeaders) {
+    req.request.headers = buildHeaders(req.rawHeaders);
+  }
+  // Asegura headers en req.connection
+  if (req.connection && !req.connection.headers && req.rawHeaders) {
+    req.connection.headers = buildHeaders(req.rawHeaders);
   }
   await ssrHandler(req, res);
 }

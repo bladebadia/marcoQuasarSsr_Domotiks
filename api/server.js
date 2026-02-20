472d31cb0e200349841c4740b81944c0d57249e3
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 19:17:24 +0000

    7

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
-import { ssrProductionExport } from '../dist/ssr/server/server-entry.js';
+import ssrHandler from '../dist/ssr/server/server-entry.js';
 
 export default async function handler(req, res) {
-  const { handler: ssrHandler } = ssrProductionExport;
   await ssrHandler(req, res);
 }

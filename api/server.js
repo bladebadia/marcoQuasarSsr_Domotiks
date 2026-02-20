6c8bb4c2c8e89c5cd1686874e2c7d317c7398f28
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 21:46:28 +0000

    22

--- api/server.js
+++ api/server.js
@@ -1,27 +1,10 @@
+import express from 'express';
 import ssrHandler from '../dist/ssr/server/server-entry.js';
 
-function ensureHeaders(req) {
-  // Construir headers desde socket.headers, rawHeaders o dejar vacío
-  let headers = req.headers;
-  if (!headers && req.socket && req.socket.headers) {
-    headers = req.socket.headers;
-  }
-  if (!headers && req.rawHeaders) {
-    headers = {};
-    for (let i = 0; i < req.rawHeaders.length; i += 2) {
-      headers[req.rawHeaders[i].toLowerCase()] = req.rawHeaders[i + 1];
-    }
-  }
-  if (!headers) headers = {};
+const app = express();
 
-  // Asignar headers en todos los subniveles posibles
-  req.headers = headers;
-  if (req.request) req.request.headers = headers;
-  if (req.connection) req.connection.headers = headers;
-  if (req.socket) req.socket.headers = headers;
-}
-
-export default async function handler(req, res) {
-  ensureHeaders(req);
+app.all('*', async (req, res) => {
   await ssrHandler(req, res);
-}
+});
+
+export default app;

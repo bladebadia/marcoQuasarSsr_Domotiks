dca682c882c11fdb37f06d69b0ce157ccf3e8aa0
bladebadia67 <miguelpicas@hotmail.com>
2026-02-20 21:23:44 +0000

    14

--- api/server.js
+++ api/server.js
@@ -1,12 +1,8 @@
 import ssrHandler from '../dist/ssr/server/server-entry.js';
 
 export default async function handler(req, res) {
-  // Adaptar req para asegurar que tiene headers (compatibilidad Vercel/Express)
-  if (!req.headers && req.getHeader) {
+  if (!req.headers) {
     req.headers = {};
-    for (const key of req.rawHeaders || []) {
-      req.headers[key.toLowerCase()] = req.getHeader(key);
-    }
   }
   await ssrHandler(req, res);
 }
